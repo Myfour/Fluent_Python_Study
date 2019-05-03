1b3777dae984120ce676f76dd1da42ae31042d0f
Myfour <oz_myx@126.com>
2019-05-04 00:06:10 +0800

    update Chapter17

---
+++ Chapter17/flags_threadpool.py
@@ -0,0 +1,23 @@
+from concurrent import futures
+from flags import save_flag, get_flag, show, main
+MAX_WORKS = 20
+
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+
+def download_many(cc_list):
+    workers = min(MAX_WORKS, len(cc_list))
+    with futures.ThreadPoolExecutor(workers) as executor:
+        res = executor.map(download_one, sorted(cc_list))
+
+    return len(list(res))
+
+
+if __name__ == '__main__':
+    for i in range(5):
+        main(download_many)
