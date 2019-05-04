1e2d8554abf4a4601c992b9e3751c645bfbf721c
myfour <oz_myx@126.com>
2019-05-04 21:21:23 +0800

    update chapter18 asyncio

---
+++ Chapter18/spinner_thread.py
@@ -0,0 +1,47 @@
+import threading
+import itertools
+import time
+import sys
+
+
+class Signal:
+    go = True
+
+
+# spin的作用就是构造一个用来画转圈的线程
+def spin(msg, signal):
+    write, flush = sys.stdout.write, sys.stdout.flush
+    for char in itertools.cycle('|/-\\'):
+        status = char + ' ' + msg
+        write(status)
+        flush()
+        write('\x08' * len(status))
+        time.sleep(.1)
+        if not signal.go:
+            break
+    write(' ' * len(status) + '\x08' * len(status))
+
+
+def slow_function():
+    time.sleep(5)
+    return 42
+
+
+def supervisor():
+    signal = Signal()
+    spinner = threading.Thread(target=spin, args=('waiting!', signal))
+    print('spinner object:', spinner)
+    spinner.start()
+    result = slow_function()
+    signal.go = False
+    spinner.join()
+    return result
+
+
+def main():
+    result = supervisor()
+    print('Answer:', result)
+
+
+if __name__ == '__main__':
+    main()
